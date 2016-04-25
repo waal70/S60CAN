7f556f36226c5461b1fdd7117ad0f1e1ff29826f
André <waal70@hotmail.com>
2016-04-25 11:22:39 +0200

    S60 clean version

--- s60can/s60can.ino
+++ s60can/s60can.ino
@@ -21,7 +21,8 @@
 ** 26-08-2015: Start of EGRMonitor branch
 ** 08-09-2015: New display, 20x4
 ** 09-09-2015: Implemented oil and boost pressure readings
-** 10-09-2015: Start of CodeCleanup branch
+** 01-02-2016: Special branch for Patrick's S80 - oil temperature & cooling temperature
+** 25-04-2016: FUNK3 branch made to reflect S80 differences (no long CAN-ID?)
 * 
 * Memory footprint (last updated: 09-09-2015):
 * Global variables use 1,432 bytes (69%) of dynamic memory, leaving 616 bytes for local variables. Maximum is 2,048 bytes.
@@ -36,11 +37,15 @@
 
 //#define DEBUG_MAIN
 //#define DEBUG_FREE_MEM
+// This enables the logging of the messages to an attached SD-card
+// Warning, although this is for SD-logging, without this define
+// still Serial initialization takes place. TODO: fix
+//#define DATALOGGER
 
 // This enables the sending of periodic keep-alive messages
 // Also pretty useful for loopback testing
 #define KEEPALIVE
-#define LOOPBACKMODE   1
+#define LOOPBACKMODE   0
 
 // This enables using the apparatus as an indepent DPF temp monitor 
 //  the original goal for making this :)
@@ -59,8 +64,13 @@
 #include <LiquidCrystal_I2C.h>
 #include "s60can_msgs.h"
 
+
 #include <SD.h>
+  #define TIME_TO_CLOSE   15
+  int fileCloseCounter =0;
+  File dataFile;
   const int chipSelect = 9; //The CARD_SC pin on the sparkfun board
+  String timeStamp;
 
 #include "usbcan.h"
 
@@ -71,6 +81,13 @@ char foo;  // for the sake of Arduino header parsing anti-automagic. Remove and 
 #define SW_VER_MAJOR  0x05    // software major version
 #define SW_VER_MINOR  0x01    // software minor version
 #define MCP2551_STANDBY_PIN A1
+
+// ONE_SHOT_MODE tries to send message only once even if error occurs during transmit.  (see MCP2515 data sheet)
+// If we are testing the Sardine CAN without any other CAN device on the network, then there will be no ACK signals acknowledging that
+// transmit succeeded and thus sending fails. If this happens, MCP2515 will keep on sending the message forever and transmit buffers will eventually
+// fill up. Also cheap ELM327 clones (with older firmware) do not support ACK-signaling, so a network consisting of MCP2515 + ELM327 does not work
+// if ONE_SHOT_MODE is not enabled. You should however disable this when connecting Sardine CAN to a car
+//#define ONE_SHOT_MODE
 
 // Filter does not pass messages by default, since VIDA seems to crash at start if all CAN messages are transmitted to it. If you are not using VIDA but
 // for example CAN Hacker, you can uncomment this define or use Lawicell 'M' and 'm' commands to set acceptance register (set mask to 0x0 to pass all messages).
@@ -115,43 +132,42 @@ static int uart_putchar (char c, FILE *stream)
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void display_operation_mode() {
+unsigned int get_operation_mode() {
 
+  uint8_t mode = mcp2515_read_register(CANCTRL);
+  uint8_t kbps = mcp2515_read_register(CNF1);
+  //for a positive ID on speed, read CNF1 and CNF2
+  // but as we limit the speeds to 125, 250 and 500,
+  // what suffices is:
+  // CNF1: 0x00: 500kbps
+  // CNF1: 0x41: 250kbps
+  // CNF1: 0x03: 125kbps
   #ifdef LCD
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print(F("mode: "));
-    switch((Canbus.getMode()))
-      {
-        case MODE_NORMAL:
-          lcd.print(F("NO"));
-          break;
-        case MODE_SLEEP:
-          lcd.print(F("SL"));
-          break;
-        case MODE_CONFIG:
-          lcd.print(F("CF"));
-          break;
-        case MODE_LISTENONLY:
-          lcd.print(F("LI"));
-          break;
-        case MODE_LOOPBACK:
-          lcd.print(F("LP"));
-          break;
-        default:
-          lcd.print(F("ER"));
-          break;
+    lcd.setCursor(14,0);
+    switch(mode)
+    {
+      case MODE_NORMAL:
+        lcd.print(F("NO"));
+        break;
+      case MODE_SLEEP:
+        lcd.print(F("SL"));
+        break;
+      case MODE_CONFIG:
+        lcd.print(F("CF"));
+        break;
+      case MODE_LISTENONLY:
+        lcd.print(F("LI"));
+        break;
+      case MODE_LOOPBACK:
+        lcd.print(F("LP"));
+        break;
+      default:
+        lcd.print(F("ER"));
+        break;
     }
     // now write the speed status line:
     lcd.setCursor (0,1);
     lcd.print(F("kbps: "));
-      //for a positive ID on speed, read CNF1 and CNF2
-      // but as we limit the speeds to 125, 250 and 500,
-      // what suffices is:
-      // CNF1: 0x00: 500kbps
-      // CNF1: 0x41: 250kbps
-      // CNF1: 0x03: 125kbps
-    uint8_t kbps = mcp2515_read_register(CNF1);
     switch (kbps)
       {
         case MCP_16MHz_125kBPS_CFG1:
@@ -167,21 +183,56 @@ void display_operation_mode() {
           lcd.print(F("???"));
           break;
       }
-    lcd.print(Canbus.getDisplayFilter());
+    //WARNING: this only works if we are setting filter mode to either
+    // 11 or 00. This will incorrectly interpret intermediate settings!
+    uint8_t fltr = mcp2515_read_register(RXB0CTRL);
+    if (!((fltr & 0x60) == 0x60))
+      lcd.print("*");
+        //one of the two bits is zero. Because of the above
+        // assumption, both bits are zero. Therefore, filter is set.
   #endif
+  return mode;
 }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int switch_mode(unsigned int mode) {
-  //Assume the worst from this mode switch
-  uint8_t ret_mode = MODE_ERROR;
 
-  if (Canbus.setMode(mode))
-    ret_mode = Canbus.getMode();
-
-  display_operation_mode();  
+  #ifdef LCD
+    lcd.clear();
+  #endif 
+  mcp2515_setCANCTRL_Mode((uint8_t)mode);
+  uint8_t ret_mode = mcp2515_read_register(CANCTRL);
   
+  get_operation_mode();  
+  
+  #ifdef LCD
+    lcd.setCursor(0,0);
+    lcd.print(F("mode: "));
+  
+    switch (ret_mode)
+      {
+        case MODE_NORMAL:
+          lcd.print(F("normal"));
+          break;
+        case MODE_SLEEP:
+          lcd.print(F("sleep"));
+          break;
+        case MODE_CONFIG:
+          lcd.print(F("config"));
+          break;
+        case MODE_LISTENONLY:
+          lcd.print(F("listen"));
+          break;
+        case MODE_LOOPBACK:
+          lcd.print(F("loopback"));
+          break;
+        default:
+          lcd.print(F("error!"));
+          return 0;
+        break;
+      }
+  #endif
   return (ret_mode == mode);
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -189,8 +240,10 @@ int switch_mode(unsigned int mode) {
 void write_DPF_msg_on_LCD (tCAN *message) {
 
   //pre-condition isDPFMessage is true (1)
+ //lcd.clearLine(0);
+ //lcd.backlight();
  lcd.setCursor(0,0);
- lcd.print(F("                "));
+ lcd.write("                ");
  lcd.setCursor(0,0);
   char temp[7]; 
   char msg[16];
@@ -200,20 +253,20 @@ void write_DPF_msg_on_LCD (tCAN *message) {
   // And calculates the temperature as follows:
   // Decimal value is temperature in tenths of degrees Kelvin. Therefore:
   // decimal value /10 - 273.15 = degrees celsius:
-  value = (uint16_t)(((message->data[5] << 8) | (message->data[6])) & 0xFFFF);
+  value = (uint8_t)(message->data[3]); // | (message->data[4])) & 0xFFFF);
 
   // Check for a valid temperature, between 0 and 2000 degrees celsius
   // Character buffers need to be at least 1 character longer than the number of characters you are writing to them
   // As we are writing 0.1 to maximum 2000.0 this means a buffer of 6+1
-  if (((double)value > 2732) && ((double)value < 22732) )
+  if (((double)value > 0) && ((double)value < 99999999) )
   {
-    dtostrf((value-2731.5)/10,4,1,temp);
+    dtostrf((value-40),4,1,temp);
     //337 is the degree symbol
-    sprintf(msg, "DPF: %s \337C", temp);
+    sprintf(msg, "KVL: %s \337C", temp);
     lcd.print(msg);
   }
   else
-    lcd.print(F("DPF: ERR \337C"));
+    lcd.print(F("KVL: ERR \337C"));
 
   lcd.setCursor(16,3);
   lcd.print(globalMessageCounter);
@@ -229,7 +282,7 @@ void write_EGR_msg_on_LCD (tCAN *message) {
  //lcd.clearLine(0);
  //lcd.backlight();
  lcd.setCursor(0,1);
- lcd.print(F("          ")); //we do this because initialization leaves characters on line 2
+ lcd.write("          "); //we do this because initialization leaves characters on line 2
  lcd.setCursor(0,1);
   char temp[6]; 
   char msg[16];
@@ -240,20 +293,20 @@ void write_EGR_msg_on_LCD (tCAN *message) {
   // And calculates the percentage as follows:
   // Decimal value is 8192 (hex: 2000)-based, meaning 8192 corresponds with 100%
   // Lower value is XXX, so the factor becomes 0.0122 (more or less)
-  value = (uint16_t)(((message->data[5] << 8) | (message->data[6])) & 0xFFFF);
+  value = (uint16_t)(((message->data[3] << 8) | (message->data[4])) & 0xFFFF);
 
   // Check for a valid value, between 0 and 8193 decimal
   // Character buffers need to be at least 1 character longer than the number of characters you are writing to them
   // As we are writing 0.1 to maximum 100.0 this means a buffer of 5+1
-  if (((double)value > 0) && ((double)value < 8193) )
+  if (((double)value > 0) && ((double)value < 3732) )
   {
-    dtostrf((double)value*factor,3,1,temp);
+    dtostrf((value-2731.5)/10,3,1,temp);
     //337 is the degree symbol
-    sprintf(msg, "EGR: %s%%", temp);
+    sprintf(msg, "TRX: %s \337C", temp);
     lcd.print(msg);
   }
   else
-    lcd.print(F("EGR: ERR %"));  
+    lcd.print(F("TRX: ERR \337C"));  
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -270,7 +323,7 @@ void write_OIL_msg_on_LCD (tCAN *message) {
   // And calculates the temperature as follows:
   // Decimal value is temperature in tenths of degrees Kelvin. Therefore:
   // decimal value / 10 - 273.15 = degrees celsius:
-  value = (uint16_t)(((message->data[5] << 8) | (message->data[6])) & 0xFFFF);
+  value = (uint16_t)(((message->data[3] << 8) | (message->data[4])) & 0xFFFF);
 
   // Check for a valid temperature, between 0 and 999.9 degrees celsius
   // Character buffers need to be at least 1 character longer than the number of characters you are writing to them
@@ -299,7 +352,7 @@ void write_BOOST_msg_on_LCD (tCAN *message) {
   // This gets the 6th and 7th element from the BOOST response message (tested through isBOOSTMessage())
   // And calculates the boost pressure as follows:
   // Decimal value is boost pressure in hectoPascals (1hPa = 1/1000 bar)
-  value = (uint16_t)(((message->data[5] << 8) | (message->data[6])) & 0xFFFF);
+  value = (uint16_t)(((message->data[4] << 8) | (message->data[5])) & 0xFFFF);
 
   // Check for a valid pressure, between 0 and 4500 hPA
   // Character buffers need to be at least 1 character longer than the number of characters you are writing to them
@@ -350,12 +403,52 @@ void show_CAN_msg_on_LCD( tCAN * message, bool recv )
 #endif
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#ifdef DATALOGGER
+void write_CAN_msg_to_file( tCAN * message, bool recv )
+{
+      char msg[32];
+      char data[8];
+      timeStamp = String(millis()); //get current timestamp
+      // Strategy is as follows:
+      // In principle, the file only closes every TIME_TO_CLOSE times
+      // So it could be, on entry of this function, that there is already a file.
+      // In that case, just log to it, otherwise, open the file.
+      File dataFile = SD.open("dl.txt", FILE_WRITE);
+      if (dataFile) {
+        if (recv)
+          dataFile.print(timeStamp + " rx ");
+        else
+          dataFile.print(timeStamp + " tx ");
+        //I do not think Volvo sends requests to remote
+        //if (message->header.rtr)
+        //    dataFile.print(F("r"));
+        //else
+        //    dataFile.print(F(" "));        
+        sprintf(msg,"%02x%02x%02x%02x|%02d|", (uint8_t)(message->id>>24),(uint8_t)((message->id>>16)&0xff),(uint8_t)((message->id>>8)&0xff),(uint8_t)(message->id&0xff),(uint8_t)(((message->id) >> 21)&0x7ff) );      
+        dataFile.print(msg);
+        for (int i=0;i<message->header.length;i++) {
+            sprintf(data,"%02x", message->data[i]);
+            dataFile.print(data);
+          }
+        dataFile.println(F("|"));
+        dataFile.close();
+      }
+}
+#endif //DATALOGGER
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int send_CAN_msg(tCAN * msg)  {   
+  printf("send_CAN_msg");
   #ifdef LCD
     //disable this for monitoring mode
     #ifndef DPFMONITOR
       write_DPF_msg_on_LCD(msg, false);
     #endif
+  #endif
+  
+  #ifdef DATALOGGER
+    write_CAN_msg_to_file(msg, false);
   #endif
   
   int ret=mcp2515_send_message_J1939(msg);  // ret=0 (buffers full), 1 or 2 = used send buffer
@@ -408,21 +501,52 @@ int init_module( unsigned long baudrate )
     sprintf(txt,"%lu ",baudrate);
     lcd.print(txt);
   #endif
-
-  if(!(Canbus.init(baudrate)))
+  
+  unsigned char mcp2515_speed;
+  switch (baudrate)
     {
-    // initialization failed!
-    #ifdef LCD
-      lcd.print(F("fail!"));
-    #endif
-    #ifdef DEBUG_MAIN
-      printf("mcp2515 init failed!");
-    #endif
-    return 0;
+      case 125000:
+        mcp2515_speed=CANSPEED_125;
+        break;
+      case 250000:
+        mcp2515_speed=CANSPEED_250;
+        break;
+      case 500000:
+        mcp2515_speed=CANSPEED_500;
+        break;
+      default:
+        #ifdef LCD
+          lcd.print(F("unsup"));
+        #endif
+        return 0;
     }
-  else
-    return 0;
 
+  if(!Canbus.init(mcp2515_speed))
+    {
+      // initialization failed!
+      #ifdef LCD
+        lcd.print(F("fail!"));
+      #endif
+      #ifdef DEBUG_MAIN
+	      printf("mcp2515 init failed!");
+      #endif
+      return 0;
+    }
+  // we need to be in config mode by default, before opening the channel
+  switch_mode(MODE_CONFIG);
+
+  // don't require interrupts from successful send
+  mcp2515_bit_modify(CANINTE, (1<<TX0IE), 0);
+
+  // enable one-shot mode
+  #ifdef ONE_SHOT_MODE
+    mcp2515_bit_modify(CANCTRL, (1<<OSM), (1<<OSM));
+  #else
+    mcp2515_bit_modify(CANCTRL, (1<<OSM), 0);
+  #endif
+  
+  // roll-over: receiving message will be moved to receive buffer 1 if buffer 0 is full
+  mcp2515_bit_modify(RXB0CTRL, (1<<BUKT), (1<<BUKT));
   
   #ifdef LCD
     lcd.setCursor(11,1);
@@ -460,12 +584,17 @@ void setFilter()
 // 03C01428: ?
 // 01E0162A: ?
   
+<<<<<<< HEAD
   uint32_t masks[2] = {0xffffffff, 0xffffffff};
   uint32_t filters[6] = {0x000FFFFE, 0x01200021, 0x00000000, 0x00000000, 0x00000000, 0x00000000};
+  
+=======
+  uint32_t masks[2] = {0x00000fff, 0x00000fff};
+  uint32_t filters[6] = {0x000007e0, 0x000007e8, 0x00000001, 0x00000002, 0x00000003, 0x00000004};
 
   delay(10);
+>>>>>>> 7e07869... ff
   mcp2515_setHWFilter(masks,2, filters, 6);
-  delay(10);
 
 
 }
@@ -483,18 +612,25 @@ void setup() {
 
   #ifdef LCD
     lcd.begin(20, 4);
+    lcd.print(F("S60 logger CAN"));
+    lcd.setCursor(0, 1);  
+    char version[16];
+    sprintf(version,"v%d.%d INIT",SW_VER_MAJOR,SW_VER_MINOR);
+    lcd.print(version);
+    //delay(1000); // a little time to read the status message
     lcd.clear();
   #endif
 
-  //Perform SPI initialization routine anyway...Precondtion: SD-card present!
+  //#ifdef DATALOGGER: perform initialization routine anyway...
     if (!SD.begin(chipSelect)) 
        Serial.println("Card NOK");
+  //#endif
 
   // we have to initialize the CAN module anyway, otherwise SPI commands (read registers/status/get_operation_mode etc) hang during invocation
   if (!init_module(500000))
     printf("NOK");
   // set MCP2551 to normal mode
-  // TODO: move to abstraction?
+ 
   pinMode(MCP2551_STANDBY_PIN,OUTPUT);
   digitalWrite(MCP2551_STANDBY_PIN,LOW);
   
@@ -513,7 +649,9 @@ void setup() {
 
   //set the filters for the messages
   switch_mode(MODE_CONFIG);
-    setFilter();
+  delay(10);
+  setFilter();
+  delay(10);
 
   //This sets DEFAULT mode:
   if (LOOPBACKMODE)
@@ -534,7 +672,7 @@ void setup() {
 
 int is_in_normal_mode()
 {
-  return (Canbus.getMode() == MODE_NORMAL);
+  return (get_operation_mode() == MODE_NORMAL);
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -561,6 +699,12 @@ void handle_CAN_rx() {
           if (isBOOSTMessage(&message))
             write_BOOST_msg_on_LCD(&message);
           
+          #ifdef DATALOGGER
+            write_CAN_msg_to_file(&message, true);
+            // check if RX buffer overflow has occured since last receive
+            //uint8_t eflg = mcp2515_read_register(EFLG);
+            //if (eflg & (1<<RX1OVR) ) -> write or log OVERFLOW
+          #endif
           #ifdef USBCAN
             return UsbCAN::dispatch_CAN_message(&message);
           #endif
@@ -629,6 +773,8 @@ void loop() {
 
   checksend_CAN_msgs();
   
+
+      
   checkRam();
 }
 
